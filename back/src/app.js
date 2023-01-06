f5812fe53513212bb215bfc510216185804099f2
luminossx092 <hehayase@gmail.com>
2023-01-06 09:10:52 -0300

    feat:post sign-up

---
+++ back/src/app.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const usuarios = []
+
+app.post('/sign-up',((req,res)=>{
+    usuarios.push(req.body)
+    res.send("OK");
+}))
+
+const PORT = 5000
+app.listen(PORT,()=>{console.log('iniciou esse servidor: ' + PORT)})
