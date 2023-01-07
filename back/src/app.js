fc13070ef579a1e3ec530d3f1699bf133f55a7f6
luminossx092 <hehayase@gmail.com>
2023-01-07 09:16:41 -0300

    feat:post tweets

--- back/src/app.js
+++ back/src/app.js
@@ -25,15 +25,22 @@ const tweets = [{
 
 app.post('/sign-up',((req,res)=>{
     usuarios.push(req.body);
-    res.send("OK");
+    res.status(201).send("OK");
 }))
 app.post('/tweets',((req,res)=>{
-    tweets.push(req.body);
-    res.send("OK");
+    const tweetReq = req.body;
+    if(!tweets.find(t=>t.username==tweetReq.username)){
+        res.status(401).send('UNAUTHORIZED')
+    }
+    tweets.push(tweetReq);
+    res.status(201).send("OK");
 }))
 app.get('/tweets',((_,res)=>{
     const arr = [];
     const lastTweets = tweets.reverse().slice(0,10)
+    if(lastTweets.length == 0){
+        res.status(404);
+    }
     for(let i = 0;i<lastTweets.length;i++){
         arr.push({...lastTweets[i],avatar: usuarios.find(u=>u.username==lastTweets[i].username).avatar});
     }
